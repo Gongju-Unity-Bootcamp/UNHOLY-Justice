faf34c8a1f01aa6bab8f86cb2243815d88b14c24
MAD_DOGGO <tkatlrdldi@gmail.com>
2024-02-16 01:24:35 +0900

    feat: Player Animation Modified

---
+++ Unholy/Assets/Scripts/Player/PlayerAnimParameter.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimParameter
+{
+    public static readonly int IsWalk = Animator.StringToHash("IsWalk");
+    public static readonly int IsSprint = Animator.StringToHash("IsSprint");
+}
