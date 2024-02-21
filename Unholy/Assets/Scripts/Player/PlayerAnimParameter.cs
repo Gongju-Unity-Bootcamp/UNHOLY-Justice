f747ed16fa24f3d93c6cfa19d58f305952424436
MAD_DOGGO <laleme0@gmail.com>
2024-02-21 17:42:54 +0900

    refactor: Player 관련 코드 리팩토링 (#67)

--- Unholy/Assets/Scripts/Player/PlayerAnimParameter.cs
+++ Unholy/Assets/Scripts/Player/PlayerAnimParameter.cs
@@ -15,8 +15,7 @@ public class PlayerAnimParameter
     public static readonly int IsDodge = Animator.StringToHash("IsDodge");
     public static readonly int IsDamage = Animator.StringToHash("IsDamage");
     public static readonly int IsAbleComboAttack = Animator.StringToHash("IsAbleComboAttack");
-    public static readonly int AbleToJump = Animator.StringToHash("AbleToJump");
-    public static readonly int AbleToDodge = Animator.StringToHash("AbleToDodge");
+    public static readonly int IsTargeting = Animator.StringToHash("IsTargeting");
     public static readonly int WeaponType = Animator.StringToHash("WeaponType");
 
     public static readonly int HorizontalMovement = Animator.StringToHash("HorizontalMovement");
---
+++ Unholy/Assets/Scripts/Player/StateBehaviour/DodgeBehaviour.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DodgeBehaviour : StateMachineBehaviour
+{
+    [Header("Component")]
+    private Rigidbody _rigidbody;
+    private Camera _playerCamera;
+    private PlayerController _playerController;
+
+    [Header("Power")]
+    private float dodgePower = 8.5f;
+
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        _playerCamera = Camera.main;
+        _rigidbody = animator.GetComponent<Rigidbody>();
+        _playerController = animator.GetComponent<PlayerController>();
+
+        Vector3 rollDirection = new Vector3(animator.transform.localRotation.x, 0, animator.transform.localRotation.z);
+
+        float horizontal = animator.GetFloat(PlayerAnimParameter.HorizontalMovement);
+        float vertical = animator.GetFloat(PlayerAnimParameter.VerticalMovement);
+
+        Vector3 moveDirection = new Vector3(horizontal, 0, vertical).normalized;
+        moveDirection.y = 0;
+
+        _playerController.isTargetingDodge = true;
+        _playerController.isDodging = true;
+
+        PlayerDodgeRotate(animator, _playerController.isRotate, moveDirection);
+        PlayerDodge(animator, rollDirection);
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //
+    //}
+
+    private void PlayerDodgeRotate(Animator animator, bool isRotate, Vector3 moveDirection)
+    {
+        if (isRotate && animator.GetBool(PlayerAnimParameter.IsTargeting))
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
+            animator.transform.rotation = Quaternion.Euler(0, _playerCamera.transform.eulerAngles.y, 0) * targetRotation;
+        }
+    }
+
+    public void PlayerDodge(Animator animator, Vector3 lastMoveDirection)
+    {
+         _rigidbody.velocity = lastMoveDirection + animator.transform.forward * dodgePower;
+
+    }
+
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        _playerController.isTargetingDodge = false;
+        _playerController.isDodging = false;
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}a
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
