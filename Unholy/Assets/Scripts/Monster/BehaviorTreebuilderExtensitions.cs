4274e740896ab235e210973b4070170500fa6345
2gangmin <gm010815@gmail.com>
2024-02-21 14:50:40 +0900

    bug: Monster(Boss) Behavior tree Rebuild(#74)
    - To reposition treeA.Tick()
    - Rename CustomAction.cs

--- Unholy/Assets/Scripts/Monster/BehaviorTreebuilderExtensitions.cs
+++ Unholy/Assets/Scripts/Monster/BehaviorTreebuilderExtensitions.cs
@@ -1,10 +1,11 @@
 using CleverCrow.Fluid.BTs.Trees;
+using System;
 
 public static class BehaviorTreeBuilderExtensions
 {
-    public static BehaviorTreeBuilder CustomAction(this BehaviorTreeBuilder builder, string statename = "Ani Name")
+    public static BehaviorTreeBuilder StateAction(this BehaviorTreeBuilder builder, string statename = "Ani Name", Action? onEnterAction = null)
     {
-        return builder.AddNode(new CustomAction(statename)
+        return builder.AddNode(new StateAction(statename, onEnterAction)
         {
             Name = statename,
         });
--- Unholy/Assets/Scripts/Monster/StateAction.cs
+++ Unholy/Assets/Scripts/Monster/StateAction.cs
@@ -1,16 +1,19 @@
 using CleverCrow.Fluid.BTs.Tasks.Actions;
 using CleverCrow.Fluid.BTs.Tasks;
 using UnityEngine;
+using System;
 
-public class CustomAction : ActionBase
+public class StateAction : ActionBase
 {
     int _animationHash;
     private bool _isEnter;
     Animator _animator;
+    Action? _onEnterAction;
 
-    public CustomAction(string stateName)
+    public StateAction(string stateName, Action? onEnterAction = null)
     {
         _animationHash = Animator.StringToHash(stateName);
+        _onEnterAction = onEnterAction;
     }
 
     protected override void OnInit()
@@ -26,8 +29,11 @@ public class CustomAction : ActionBase
             _isEnter = true;
             _animator.Play(_animationHash, 0, 0);
 
+            _onEnterAction?.Invoke();
+
             return TaskStatus.Continue;
         }
+
 
         if (_animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1f)
         {
