a31de7d40818e1b2c01a6c630725ae855c4ec249
MAD_DOGGO <laleme0@gmail.com>
2024-02-22 19:59:21 +0900

    feat: Player Weapon Switch FSM (#38)

--- Unholy/Assets/Scripts/Player/WeaponSwitch.cs
+++ Unholy/Assets/Scripts/Player/WeaponSwitch.cs
@@ -7,6 +7,15 @@ public class WeaponSwitch : MonoBehaviour
 {
     [Header("Component")]
     private Animator _animator;
+    public Transform _twoHand;
+    public Transform _leftHandAttachPoint;
+
+    [Header("IK")]
+    [Range(0, 1)] public float leftHandPositionWeight;
+    [Range(0, 1)] public float leftHandRotationWeight;
+    private Transform blendToTransform;
+    private const float DELAYTIME = 0.75f;
+
 
     [Header("Weapon Field")]
     // 무기 미 장착으로 시작하기에 인덱스 num이 -1부터 시작
@@ -16,6 +25,7 @@ public class WeaponSwitch : MonoBehaviour
     private void Awake()
     {
         _animator = GetComponent<Animator>();
+        blendToTransform = _twoHand.transform.GetChild(0);
 
         if (weaponIndex < 0)
             GetIndexOfWeaponTypes(WeaponType.Unarmed);
@@ -24,6 +34,56 @@ public class WeaponSwitch : MonoBehaviour
     private void Update()
     {
         _animator.SetInteger(PlayerAnimParameter.WeaponType, weaponIndex);
+    }
+
+    private void OnAnimatorIK(int layerIndex)
+    {
+        if(weaponIndex == 3)
+        {
+            _animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, leftHandPositionWeight);
+            _animator.SetIKRotationWeight(AvatarIKGoal.LeftHand, leftHandRotationWeight);
+
+            _animator.SetIKPosition(AvatarIKGoal.LeftHand, _leftHandAttachPoint.position);
+            _animator.SetIKRotation(AvatarIKGoal.LeftHand, _leftHandAttachPoint.rotation);
+        }
+    }
+
+    public void BlendIK(float timeToBlend, int weaponIndex)
+    {
+        StopAllCoroutines();
+        StartCoroutine(_BlendIK(timeToBlend, weaponIndex));
+    }
+
+    public IEnumerator _BlendIK(float timeToBlend, int weaponIndex)
+    {
+        var t = 0f;
+        var blendTo = 0;
+        var blendFrom = 0;
+        
+        if(weaponIndex == 3)
+        {
+            leftHandPositionWeight = 0;
+            leftHandRotationWeight = 0;
+        }
+        else
+        {
+            leftHandPositionWeight = 1;
+            leftHandRotationWeight = 1;
+        }
+
+        yield return new WaitForSeconds(DELAYTIME);
+
+        if (weaponIndex == 3) blendTo = 1;
+        else blendFrom = 1;
+        
+        while(t < 1)
+        {
+            t += Time.deltaTime / timeToBlend;
+            _leftHandAttachPoint = blendToTransform;
+            leftHandPositionWeight = Mathf.Lerp(blendFrom, blendTo, t);
+            leftHandRotationWeight = Mathf.Lerp(blendFrom, blendTo, t);
+            yield return null;
+        }
     }
 
     /// <summary>
@@ -35,13 +95,17 @@ public class WeaponSwitch : MonoBehaviour
         switch(currentType)
         {
             case WeaponType.Unarmed:
-                weaponIndex = 1; break;
+                weaponIndex = 1;
+                break;
             case WeaponType.OneHand:
-                weaponIndex = 2; break;
+                weaponIndex = 2;
+                break;
             case WeaponType.TwoHand:
-                weaponIndex = 3; break;
+                weaponIndex = 3;
+                break;
             case WeaponType.Bow:
-                weaponIndex = 4; break;
+                weaponIndex = 4;
+                break;
             default:
                 weaponIndex = 0; break;
         }
