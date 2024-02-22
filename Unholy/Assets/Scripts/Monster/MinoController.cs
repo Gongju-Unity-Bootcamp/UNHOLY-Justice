81517bf3f404653d288792caffeddbf196b1a8ad
JJARSANGKIM <kjanicat@gmail.com>
2024-02-22 15:13:42 +0900

    feat: Add Monster(Boss) NavMeshAgent
    - Add Unity Package AI Navigation
    - Monsters follwing players with NavMeshAgent

--- Unholy/Assets/Scripts/Monster/MinoController.cs
+++ Unholy/Assets/Scripts/Monster/MinoController.cs
@@ -2,39 +2,56 @@ using CleverCrow.Fluid.BTs.Trees;
 using CleverCrow.Fluid.BTs.Tasks;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.AI;
 
 
 public class MinoController : MonoBehaviour
 {
+    [SerializeField] private BehaviorTree _treeA;
+    [SerializeField] Transform _player;
+
     [SerializeField] Vector3 _backStepPower = new Vector3(0f, 2.5f, 0f);
     [SerializeField] float _backstepSpeed = 5.5f;
 
-    [SerializeField] private BehaviorTree _treeA;
-    public bool _isIdle;   
-    public bool _attackRange = false;
-    public bool _backPOS = false;
+    [SerializeField] Vector3 _jumpPower = new Vector3(0f, 2.5f, 0f);
+    [SerializeField] float _jumpSpeed = 5.5f;
+
+    public bool _isIdle; // true 일시 Idle 상태 해제
     public bool _keepDEF = false;
-    public bool _out7SEC = false;
+    public bool _isForward = false; // 앞 뒤 구분
 
     public float _bossHP = 10f;
+    public float _dist;
+    public float _distance;
+    public float _attackRange = 6f;
+    public float _minJump = 15f;
+    public float _maxJump = 20f;
 
     Animator _bossAnimator;
     Rigidbody _rigidbody;
+    NavMeshAgent _agent;
+    public float _meleeAttackRange = 4f;
 
-    
+
 
     private void Awake()
     {
         _bossAnimator = GetComponent<Animator>();
         _rigidbody = GetComponent<Rigidbody>();
+        _agent = GetComponent<NavMeshAgent>();
 
         _treeA = new BehaviorTreeBuilder(gameObject)
             .Selector()
                 .Sequence()
-                    .Condition("attackRange", () => _attackRange == true)
+                    .Condition("attackRange", () => _dist <= _attackRange)
                     .Selector()
                         .Sequence()
-                            .Condition("backPOS", () => _backPOS == true)
+                            .Condition("backPOS", () => _isForward == true)
+                            .Do(() =>
+                            {
+                                _agent.isStopped = true;
+                                return TaskStatus.Success;
+                            })
                             .StateAction("BossBackstep", ProcessBackstep)
                             .StateAction("BossStompAttack")
                         .End()
@@ -51,16 +68,39 @@ public class MinoController : MonoBehaviour
                     .End()
                 .End()
                 .Sequence()
-                    .Condition("out7SEC", () => _out7SEC == true)
-                    .StateAction("BossJumpAttack")
+                    .Condition("out7SEC", () => _dist >= _minJump && _dist < _maxJump)
+                    .Do(() =>
+                    {
+                        _agent.isStopped = true;
+                        return TaskStatus.Success;
+                    })
+                    .StateAction("BossJumpAttack", ProcessJumpAttack)
                 .End()
-                .Do("BossTrack", () =>
-                {
-                    _bossAnimator.Play("BossTrack");
-                    return TaskStatus.Success;
-                })
+                .RepeatUntilSuccess()
+                    .Do("BossTrack", () =>
+                    {
+                        _bossAnimator.Play("BossTrack");
+                        _agent.isStopped = false;
+                        _agent.SetDestination(_player.position);
+                        if (_dist > 5f)
+                        {
+                            return TaskStatus.Success;
+                        }
+                        else
+                        {
+                            _agent.isStopped = true;
+                            return TaskStatus.Failure;
+                        }
+                    })
+                .End()
             .End()
             .Build();
+    }
+
+
+    private void Update()
+    {
+        _checkDistance();
     }
 
     private void OnEnable()
@@ -115,9 +155,35 @@ public class MinoController : MonoBehaviour
 
     public void ProcessBackstep()
     {
-
+        transform.LookAt(_player.position);
         var localPower = transform.TransformVector(_backStepPower);
         _rigidbody.AddForce(localPower, ForceMode.Impulse);
         _rigidbody.velocity = _backstepSpeed * -_rigidbody.transform.forward;
+    }
+
+    public void ProcessJumpAttack()
+    {
+        transform.LookAt(_player.position);
+        var localPower = transform.TransformVector(_jumpPower);
+        _rigidbody.AddForce(localPower, ForceMode.Impulse);
+        _rigidbody.velocity = _jumpSpeed * _rigidbody.transform.forward;
+    }
+    // 플레이어와의 위치 계산
+    private void _checkDistance()
+    {
+        _dist = Vector3.Distance(_player.position, transform.position);
+        Vector3 directionToPlayer = (_player.position - transform.position).normalized;
+        Vector3 bossForward = transform.forward;
+
+        float dotProduct = Vector3.Dot(directionToPlayer, bossForward);
+
+        if (dotProduct > 0.5f)
+        {
+            _isForward = false;
+        }
+        else
+        {
+            _isForward = true;
+        }
     }
 }
